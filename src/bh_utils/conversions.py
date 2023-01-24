629c10905691ca9c4103f4bf3a984a89187b6769
behai-nguyen <behai_nguyen@hotmail.com>
2023-01-24 21:50:01 +1100

    Updated module with documentation, added and new modules.

--- src/bh_utils/conversions.py
+++ src/bh_utils/conversions.py
@@ -1,48 +1,63 @@
+"""Some custom conversion functions.
+
+For usage examples, see ``./tests/test_conversions.py``.
 """
-Some basic conversion functions.
-"""
-def str_to_bool( str_val: str, exception_msg=None ) -> bool:
-    """
-    Converts a string to Boolean.
+from typing import Union
+
+def str_to_bool(str_val: str, exception_msg=None) -> Union[bool, None]:
+    """Convert a string to Boolean.
 	
-    Main reference: 
-    https://stackoverflow.com/questions/715417/converting-from-a-string-to-boolean-in-python
+    :Reference: 
+    
+        * `Converting from a string to boolean in Python \
+            <https://stackoverflow.com/questions/715417/converting-from-a-string-to-boolean-in-python>`_
 
-    If str_val can be converted to Boolean, then returns the Boolean value.
-    Otherwise: 1. returns None if exception_msg is None. 2. Raises exception otherwise.
+    If str_val can be converted to Boolean, then return a Boolean value. Otherwise, 
+    return None if exception_msg is None; raise an exception otherwise.
 	
-    This method is intentional. It is not an overlook because I don't know of 
-    distutils.util.strtobool( ... ). I want this particular implementation.
+    The behaviour of this function is different to the standard function 
+    `distutils.util.strtobool(val) <https://docs.python.org/3/distutils/apiref.html?highlight=strtobool#distutils.util.strtobool>`_.
 
-    Return:
+    :param str str_val: a string to be converted to Boolean.
+    :param str exception_msg: the exception message to be raised if str_val can't be converted to Boolean.
 
-    Four ( 4 ) possibilities: True, False, None or raise an exception.
+    :return: a Boolean if str_val can be converted. None if can't be converted and exception_msg is None.
+    :rtype: Bool or None.
+
+    :raises Exception: if str_val can not be converted and exception_msg is not None.
     """
 	
     res = None
 
-    if str_val.lower() in [ 'true', '1', 't', 'yes', 'y', 'on' ]: 
+    if str_val.lower() in ['true', '1', 't', 'yes', 'y', 'on']: 
         res = True
-    elif str_val.lower() in [ 'false', '0', 'f', 'no', 'n', 'off' ]: 
+    elif str_val.lower() in ['false', '0', 'f', 'no', 'n', 'off']: 
         res = False
 
     # Succeeds.
-    if ( res != None ): return res
+    if (res != None): return res
 
     # Not succeeded. No exception required. Returns None.
-    if ( exception_msg == None ): return res
+    if (exception_msg == None): return res
 
     # Not succeeded. Exception required: raise exception.
-    if ( res == None ): raise Exception( exception_msg )
+    if (res == None): raise Exception(exception_msg)
 	
-def is_integer( value ) -> bool:
+def is_integer(value: Union[int, str]) -> bool:
     """
-    Checks whether the value of a variable is an integer.
+    Check whether the value of a variable is an integer or can be converted to an integer.
 
-    Main reference: 	
-    https://stackoverflow.com/questions/3501382/checking-whether-a-variable-is-an-integer-or-not
+    :Reference: 
+    
+        * `Checking whether a variable is an integer or not \
+            <https://stackoverflow.com/questions/3501382/checking-whether-a-variable-is-an-integer-or-not>`_
+
+    :param [int, str] value: value to be checked.
+
+    :return: True if value is an integer or can be converted to an integer. False otherwise.
+    :rtype: Bool.
     """	
-    if ( isinstance(value, int) ): return True
-    if ( isinstance(value, str) ):
+    if (isinstance(value, int)): return True
+    if (isinstance(value, str)):
         if value.isnumeric(): return True
     return False
