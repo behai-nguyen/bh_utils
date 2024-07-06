c57ebf86809c397641ad59f4be33b24f31f0bab3
behai-nguyen <behai_nguyen@hotmail.com>
2024-07-06 10:58:02 +1000

    Bug Fixed: removed logger configuration.
    Added logger with NullHandler.
    Updated as follows:
        Uses NullHandler default logger.
        Modified def get_template(...) removed logger: Logger param.

--- src/bh_utils/template_funcs.py
+++ src/bh_utils/template_funcs.py
@@ -6,17 +6,19 @@ For usage examples, see ``./tests/test_template_funcs.py``.
 
 from os import (
     getcwd,
+    sep,
     path,
 )
-
-from logging import Logger
 
 from jinja2.environment import (
     Template,
     Environment,
 )
 
-from bh_utils import BH_TEMPLATE_FILE_NOT_FOUND_MSG
+from bh_utils import (
+    BH_TEMPLATE_FILE_NOT_FOUND_MSG,
+    logger,
+)
 
 def template_root_path(source_dir="src") -> str:
     """Get a project template root path.
@@ -61,8 +63,12 @@ def template_root_path(source_dir="src") -> str:
 
     drive, root_path = path.splitdrive(base_dir)
 
-    paths = root_path.split("/")
-    project_path = paths[len(paths)-1][1:] if len(drive) > 0 else paths[len(paths)-1]
+    paths = root_path.split(sep)
+    project_path = paths[len(paths)-1] if len(drive) > 0 else paths[len(paths)-1]
+
+    # print("\nbase_dir: ", base_dir, ", source_dir: ", source_dir, ", project_path: ", project_path, "\n")
+
+    logger.debug(f"base_dir: {base_dir}, source_dir: {source_dir}, project_path: {project_path}")
 
     return path.join(base_dir, source_dir, project_path, "templates")
 
@@ -86,7 +92,7 @@ def template_path(template_dir: str, source_dir="src"):
 
     return path.join(template_root_path(source_dir), template_dir)
 
-def get_template(template_env: Environment, file_name: str, logger: Logger) -> Template:
+def get_template(template_env: Environment, file_name: str) -> Template:
     """Attempt to load a template.
 
     :param template_env: an already prepared valid Jinja Environment class.
@@ -95,7 +101,6 @@ def get_template(template_env: Environment, file_name: str, logger: Logger) -> T
     :param str file_name: a template file name to load.
 
     :param logger: where to log possible exception to.
-    :type logger: `logging.Logger <https://docs.python.org/3/library/logging.html#logger-objects>`_
 
     :return: `jinja2.Template <https://jinja.palletsprojects.com/en/3.0.x/api/#jinja2.Template>`_.
     """
@@ -106,5 +111,6 @@ def get_template(template_env: Environment, file_name: str, logger: Logger) -> T
         
     except Exception as e:
         msg = BH_TEMPLATE_FILE_NOT_FOUND_MSG.format(path.join(template_env.loader.searchpath[0], file_name))
-        logger.exception(msg)
+        if logger != None:
+            logger.exception(msg)
         raise Exception(msg)
